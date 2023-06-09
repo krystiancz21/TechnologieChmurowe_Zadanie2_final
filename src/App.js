4e94e5cbb6f80176b3ab77c4acdbfd4cfbf8595f
krystiancz21 <krystianczapla2000@gmail.com>
2023-06-09 15:39:04 +0200

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Krystian Czapla - Ti 6.1
+          Krystian Czapla - Ti 6.1 - Informatyka
         </p>
         <a
           className="App-link"
